2c8ff0d2587441810d65702989ddc9f4546c0707
zabbat <zabbat@jenkins-ubuntu.com>
2018-07-23 10:22:35 +0100

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 pipeline {
 	agent any
+	options{skipDefaultCheckput()}
 	stages {
 		stage('pipeline'){
 			steps{
+				checkout scm
 				echo 'running'
 				echo 'pass'
 			}
